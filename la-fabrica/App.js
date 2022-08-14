bc131a787cdb9cf46bffe0bcc57a07acd7869ae3
enrique.gomez <enrique.gomez@bq.com>
2022-08-14 18:48:36 +0200

    calculate and update rssi on scan stop

--- la-fabrica/App.js
+++ la-fabrica/App.js
@@ -7,13 +7,14 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 const BleManagerModule = NativeModules.BleManager;
 const bleManagerEmitter = new NativeEventEmitter(BleManagerModule);
 
+var measured_rssi = []
+
 export default function App() {
 
   const [bluetooth_started, setBluetoothStarted] = useState(false)
   const [beacon_rssi, setBeaconRssi] = useState(0)
 
   useEffect(() => {
-    
     if (!bluetooth_started) {
       PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION)
       .then((result) => {
@@ -43,20 +44,27 @@ export default function App() {
       .then(() => {
         console.log("Module initialized");
         bleManagerEmitter.addListener('BleManagerDiscoverPeripheral', handleDiscoverPeripheral)
+        bleManagerEmitter.addListener('BleManagerStopScan', handleStopScan)
         setBluetoothStarted(true)
       });   
     }
-  })
+
+    return (() => {
+      bleManagerEmitter.removeAllListeners('BleManagerDiscoverPeripheral');
+      bleManagerEmitter.removeAllListeners('BleManagerStopScan' );
+    })
+  }, [])
 
   useEffect(() => {
     if (bluetooth_started) {
       var find_beacon = setInterval(() => {
         console.log("I am lokking for you")
+        measured_rssi = []
         BleManager.scan([], 1, false)
         .then(() => {
           console.log("Done scanning")
         })
-      }, 10000);
+      }, 5000);
     }
     
     return () => clearInterval(find_beacon)
@@ -66,14 +74,24 @@ export default function App() {
     const beacon_mac = await AsyncStorage.getItem("beacon_mac")
     if (peripheral.id == beacon_mac) {
       console.log('Beacon RSSI', peripheral.rssi);
-      if (beacon_rssi !== 0) {
-        rssi = (peripheral.rssi + beacon_rssi)/2
-        rssi = math.round(rssi)
-      } else {
-        rssi = peripheral.rssi
-      }
-      setBeaconRssi(rssi)
+      measured_rssi.push(peripheral.rssi)
+      
     }
+  }
+
+  const handleStopScan = () => {
+    console.log(measured_rssi)
+    var rssi = 0
+    for ( var i = 0; i < measured_rssi.length; i ++) {
+      if (rssi !== 0) {
+        rssi = (measured_rssi[i] + rssi)/2
+        rssi = Math.round(rssi)
+      } else {
+        rssi = measured_rssi[i]
+      }
+    }
+    setBeaconRssi(rssi)
+    console.log("rssi updated")
   }
 
   return (
