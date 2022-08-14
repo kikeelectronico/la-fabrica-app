83bbbad83740411418b18191eeee355a091c3856
enrique.gomez <enrique.gomez@bq.com>
2022-08-14 14:36:58 +0200

    add beacon rssi state and interval

--- la-fabrica/App.js
+++ la-fabrica/App.js
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const [beacon_rssi, setBeaconRssi] = useState(0)
+
+  useEffect(() => {
+    const find_beacon = setInterval(() => {
+      console.log("I am lokking for you")
+    }, 50000);
+
+    return clearInterval(find_beacon)
+  })
+
   return (
     <View style={styles.container}>
       <Text style={styles.main_text}>La fábrica App</Text>
+      <Text style={styles.beacon_rssi}>{beacon_rssi} dBm</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -20,5 +32,8 @@ const styles = StyleSheet.create({
   },
   main_text: {
     fontSize: 25,
-  }
+  },
+  beacon_rssi: {
+    fontSize: 40,
+  },
 });
